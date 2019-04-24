e102603e4bd8d8d53b2cb29ab970cc9bfc80b5a1
ZhiFeng Hu <hufeng1987@gmail.com>
2019-04-24 11:53:12 +0800

    Update files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+cp -rf docker-data/redis-cluster1.tmpl.before docker-data/redis-cluster1.tmpl
+cp -rf docker-data/redis-cluster2.tmpl.before docker-data/redis-cluster1.tmpl
+cp -rf docker-data/redis-cluster3.tmpl.before docker-data/redis-cluster1.tmpl
+docker-compose up -d
 docker-compose exec redis1  redis-cli -p 7000 --cluster create 173.17.0.2:7000 173.17.0.3:7001 173.17.0.4:7002 --cluster-replicas 0
+cp -rf docker-data/redis-cluster1.tmpl.after docker-data/redis-cluster1.tmpl
+cp -rf docker-data/redis-cluster2.tmpl.after docker-data/redis-cluster1.tmpl
+cp -rf docker-data/redis-cluster3.tmpl.after docker-data/redis-cluster1.tmpl
